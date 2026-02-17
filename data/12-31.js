80b898ede97c86b3ae4897d75c6146b06b4d46ec
Vakhtang Jincharadze <162159990+vakho-jin@users.noreply.github.com>
2026-02-17 13:20:39 +0400

    add data/ .js files

---
+++ data/12-31.js
@@ -0,0 +1,4 @@
+window.__dayDataCallback__([
+  {"name":"Новогодний вечер","fact":"Традиция встречать Новый год под бой кремлёвских курантов по телевизору оформилась в СССР в 1970-х годах.","img":"new year eve countdown champagne celebration"},
+  {"name":"Последний день года","fact":"31 декабря — день подведения итогов. В России в этот день сжигают записки с желаниями и пьют шампанское.","img":"fireworks midnight celebration winter sky"}
+]);
