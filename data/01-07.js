bf49d61406716d5ac8b1a91e645ae1a21af27874
Vakhtang Jincharadze <162159990+vakho-jin@users.noreply.github.com>
2026-02-27 16:13:24 +0400

    add some days new format
    add new formatted days january 1, 2, 3, 4, 5, 6 and february 27, 28, 29, march 1

--- data/01-07.js
+++ data/01-07.js
@@ -2,6 +2,6 @@ window.__dayDataCallback__([
   {
   "name": "შობა",
   "fact": "მართლმადიდებლური შობა საქართველოში. ტაძრებში ღამისთევის წირვა-ლოცვა აღევლინება, ხოლო 7 იანვარს ოჯახები სადღესასწაულო ტრაპეზზე იკრიბებიან [citation:2][citation:3][citation:5].",
-  "img": "https://unsplash.com/s?query=orthodox+christmas+georgia+tbilisi"
+  "img": "christmas"
 }
 ]);
