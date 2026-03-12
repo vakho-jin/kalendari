bd51411acecd68edb7bb4af4f7fb6f086f36b1cf
Vakhtang Jincharadze <162159990+vakho-jin@users.noreply.github.com>
2026-03-12 11:09:16 +0400

    Delete new_data_format.js

--- new_data_format.js
+++
@@ -1,6 +0,0 @@
-window.__dayDataCallback__({
-  holidays: [ { name: "...", fact: "...", img: "..." } ],
-  history:  [ { name: "...", fact: "...", img: "..." } ],
-  born:     [ { name: "...", fact: "...", img: "..." } ],
-  died:     [ { name: "...", fact: "...", img: "..." } ]
-})
